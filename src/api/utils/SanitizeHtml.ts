db270bf61d591fd24033b125a68ada17984741ce
Remco Epicnabbo <remcovlogs@gmail.com>
2026-05-16 18:45:56 +0200

    Disallow style attribute in HTML sanitizer
    Remove 'style' from the DOMPurify ALLOWED_ATTR list to prevent inline styles from being preserved during sanitization. This tightens the sanitizer policy while keeping href, target, class, src and alt allowed to maintain necessary markup for links and images.

--- src/api/utils/SanitizeHtml.ts
+++ src/api/utils/SanitizeHtml.ts
@@ -4,7 +4,7 @@ export const SanitizeHtml = (html: string): string =>
 {
     return DOMPurify.sanitize(html, {
         ALLOWED_TAGS: [ 'b', 'i', 'u', 'br', 'span', 'div', 'p', 'a', 'strong', 'em', 'img' ],
-        ALLOWED_ATTR: [ 'href', 'target', 'class', 'style', 'src', 'alt' ],
+        ALLOWED_ATTR: [ 'href', 'target', 'class', 'src', 'alt' ],
         ALLOW_DATA_ATTR: false
     });
 };
